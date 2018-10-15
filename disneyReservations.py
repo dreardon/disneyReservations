85678e7709ca8b15a002502eb27a4958a6a73c1e
Daniel P. Reardon <danreardon@gmail.com>
2018-10-15 06:00:43 -0400

    Initial commit

---
+++ disneyReservations.py
@@ -0,0 +1,77 @@
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+import os
+import time 
+import boto3
+
+def disneyReservation(event):
+    partyTime = event['partyTime']
+    partySize = event['partySize']
+    reservationDate = event['reservationDate']
+    chrome_options = Options()  
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-setuid-sandbox")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--incognito")
+    chrome_options.add_argument("--disable-application-cache")
+    chrome_options.add_argument("--test-type")
+    chrome_options.add_argument("--js-flags=--expose-gc")
+    chrome_options.add_argument("--enable-precise-memory-info")
+    chrome_options.add_argument("--disable-default-apps")
+    chrome_options.add_argument("--window-size=1024,1000")
+    chrome_options.binary_location = "./bin/headless-chromium"
+
+    #Setup
+    driver = webdriver.Chrome(executable_path=('./bin/chromedriver'), options=chrome_options)  
+    driver.get('https://disneyworld.disney.go.com/dining/magic-kingdom/cinderella-royal-table/')
+    print("Got Page")
+
+    #Select Party Size
+    driver.find_element_by_id("partySize-wrapper").click();
+    time.sleep(5)
+    xpath_sizeLoc = '''//li[@data-value=\"''' +partySize+ '''\"]'''
+    driver.find_element_by_xpath(xpath_sizeLoc).click();
+    print("Finished Party Size")
+
+    #Select Time
+    driver.find_element_by_id("searchTime-wrapper").click();
+    time.sleep(5)
+    xpath_timeLoc = '''//li[@data-display=\"''' +partyTime+ '''\"]'''
+    driver.find_element_by_xpath(xpath_timeLoc).click();
+    print("Finished Time")
+
+    #Select Date
+    date = driver.find_element_by_id("diningAvailabilityForm-searchDate");
+    time.sleep(10);
+    date.send_keys(Keys.CONTROL + "a");
+    date.send_keys(Keys.DELETE);
+    date.send_keys(reservationDate);
+    driver.find_element_by_id("checkAvailability").click();
+    print("Finished Date")
+    
+    #Determine Availability
+    driver.find_element_by_id("dineAvailSearchButton").click()
+    time.sleep(10);
+    try:
+        results = driver.find_element_by_class_name("ctaNoAvailableTimesContainer")
+    except:
+        results = driver.find_element_by_class_name("ctaAvailableTimesContainer")
+        client = boto3.client('sns')
+        response = client.publish(
+            TargetArn='arn:aws:sns:us-east-1:679695450108:DisneyRes',
+            Message=json.dumps({'default': 'Default Message',
+                                'sms': "Available Time at Cinderella's Royal Table for: " + partySize + ' people on ' + reservationDate + ' at ' + results.text,
+                                'email': "Available Time at Cinderella's Royal Table for: " + partySize + ' people on ' + reservationDate + ' at ' + results.text}),
+            Subject='A New Reservation is Available',
+            MessageStructure='json'
+        )
+
+    print(results.text)
+    print("Done")
+    driver.close()
+
+event={"partyTime" : "breakfast","partySize" : "2","reservationDate" : "02/09/2019"}
+disneyReservation(event)
---
+++ setup.sh
@@ -0,0 +1,16 @@
+mkdir -p bin/
+
+# Get chromedriver
+curl -SL https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip > chromedriver.zip
+unzip chromedriver.zip -d ./bin/
+
+# Get Headless-chrome
+curl -SL https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-37/stable-headless-chromium-amazonlinux-2017-03.zip > headless-chromium.zip
+unzip headless-chromium.zip -d ./bin/
+
+# Clean
+rm headless-chromium.zip chromedriver.zip
+
+virtualenv venvs
+source venvs/bin/activate
+pip install -r requirements.txt
